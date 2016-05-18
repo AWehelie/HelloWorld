0a3e55a3edecc5579d516f6e9f2698a0909aa755
Abdikani Wehelie <a.wehelie@hotmail.nl>
2016-05-18 11:00:42 +0200

    HttpClient is not supported any more in sdk 23. You have to use URLConnection or downgrade to sdk 22 (compile 'com.android.support:appcompat-v7:22.2.0')  ( FIXED ) gradle sdk target nu is 23

--- app/src/main/java/com/example/awehelie/helloworld/WebViewActivity.java
+++ app/src/main/java/com/example/awehelie/helloworld/WebViewActivity.java
@@ -1,7 +1,36 @@
 package com.example.awehelie.helloworld;
 
-/**
- * Created by AWehelie on 12/05/16.
- */
-public class WebViewActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new MyWebViewClient());
+
+        Bundle bundle = this.getIntent().getExtras();
+        String url = bundle.getString("url");
+
+        if (null != url) {
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.loadUrl(url);
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
 }
